c3151894f692a7459390b411908ddc6e4bf801d0
Brian Hopkins <brianhh1230@gmail.com>
2017-08-28 11:56:17 -0400

    Update __init__.py

--- mycroftapi/__init__.py
+++ mycroftapi/__init__.py
@@ -14,11 +14,8 @@ class MycroftAPI(object):
     def speak_text(self, text):
         mycroft_speak = ('"{}"'.format(text))
         mycroft_type = '"speak"'
-        mycroft_data = '{"expect_response": false, 
-                        "utterance": %s}, 
-                        "context": null' % mycroft_speak
-        message = '{"type": ' + mycroft_type + ', 
-                    "data": ' + mycroft_data + '}'
+        mycroft_data = '{"expect_response": false, "utterance": %s}, "context": null' % mycroft_speak
+        message = '{"type": ' + mycroft_type + ', "data": ' + mycroft_data + '}'
         self._ws.send(message)
         response = "Message Sent to Mycroft Instance: {}".format(self.mycroft_ip)
         return response
