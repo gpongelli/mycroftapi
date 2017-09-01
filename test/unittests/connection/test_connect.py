de1b471adfb6ff7cdc47bb074d02574e4c70c448
Brian Hopkins <brianhh1230@gmail.com>
2017-09-01 19:52:39 -0400

    updating

--- test/unittests/connection/test_connect.py
+++ test/unittests/connection/test_connect.py
@@ -4,10 +4,6 @@ from mycroftapi import MycroftAPI
 
 
 class TestSet(unittest.TestCase):
-
-    def __init__(self):
-        self.ws = MycroftAPI()
-
     def test_ws_connection(self):
         m = MycroftAPI(mycroft_ip='127.0.0.1')
         m.__init__ = MagicMock(name='connection')
