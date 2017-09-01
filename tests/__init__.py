f9c58c311e85f955a99ac99e5d84ffefe5741604
Brian Hopkins <brianhh1230@gmail.com>
2017-09-01 18:14:12 -0400

    fixing mistake

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,7 @@ from mycroftapi import MycroftAPI
 class TestSet(unittest.TestCase):
     def test_ws_connection(self):
         m = MycroftAPI(mycroft_ip='127.0.0.1', text='hello brian')
-        m.open_ws_connection = MagicMock(name='connection')
+        m.__init__ = MagicMock(name='connection')
         ws = MycroftAPI(m.mycroft_ip, m.text)
         self.assertEqual(ws.mycroft_ip, ('127.0.0.1'))
         self.assertEqual(ws.text, ('hello brian'))
