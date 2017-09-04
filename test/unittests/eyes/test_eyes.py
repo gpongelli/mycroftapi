a8f1827d2ddbc16fde0acf42ca8f8f8400a88db2
Brian Hopkins <brianhh1230@gmail.com>
2017-09-04 07:44:13 -0400

    add bright test

--- test/unittests/eyes/test_eyes.py
+++ test/unittests/eyes/test_eyes.py
@@ -128,5 +128,26 @@ class TestSet(unittest.TestCase):
         m.eyes_color(r, g, b)
         self.assertEqual(message, mock_ws.message)
 
+    @patch('mycroftapi.create_connection')
+    def test_eyes_bright(self, mock_create_conn):
+        # Create simple replacement websocket object and return it
+        # when creating sockets
+        mock_ws = MockWS()
+        mock_create_conn.return_value = mock_ws
+        # Test that init calls create_connection with correct param
+        m = MycroftAPI('127.0.0.1')
+        mock_create_conn.assert_called_with(
+            "ws://" + '127.0.0.1' + ":8181/core")
+        # Check that message bus message looks like what we expect
+        # Expected data to websocket
+        level = 20
+        mycroft_type = '"enclosure.eyes.level"'
+        mycroft_data = '{"level": %s}, ' \
+                       '"context": null' % level
+        message = '{"type": ' + mycroft_type + \
+                  ', "data": ' + mycroft_data + '}'
+        m.eyes_brightness(level)
+        self.assertEqual(message, mock_ws.message)
+
 if __name__ == '__main__':
     unittest.main()
