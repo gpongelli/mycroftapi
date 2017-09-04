937ace6ab6a0a4be90a553e8ead1b18e8f6bb397
Brian Hopkins <brianhh1230@gmail.com>
2017-09-04 07:34:04 -0400

    add eyes look test

--- test/unittests/eyes/test_eyes.py
+++ test/unittests/eyes/test_eyes.py
@@ -84,5 +84,26 @@ class TestSet(unittest.TestCase):
         m.squint_eyes()
         self.assertEqual(message, mock_ws.message)
 
+    @patch('mycroftapi.create_connection')
+    def test_eyes_look(self, mock_create_conn):
+        # Create simple replacement websocket object and return it
+        # when creating sockets
+        mock_ws = MockWS()
+        mock_create_conn.return_value = mock_ws
+        # Test that init calls create_connection with correct param
+        m = MycroftAPI('127.0.0.1')
+        mock_create_conn.assert_called_with(
+            "ws://" + '127.0.0.1' + ":8181/core")
+        # Check that message bus message looks like what we expect
+        # Expected data to websocket
+        side = "u"
+        mycroft_type = '"enclosure.eyes.look"'
+        mycroft_data = '{"side": "%s"}, ' \
+                       '"context": null' % side
+        message = '{"type": ' + mycroft_type + \
+                  ', "data": ' + mycroft_data + '}'
+        m.eyes_look(side)
+        self.assertEqual(message, mock_ws.message)
+
 if __name__ == '__main__':
     unittest.main()
