ab75087b1c0c0a4b31346ab22194c8d7239060c1
Brian Hopkins <brianhh1230@gmail.com>
2017-09-04 07:25:36 -0400

    add more tests
    add eyes_on test coverage

--- test/unittests/eyes/test_eyes.py
+++ test/unittests/eyes/test_eyes.py
@@ -50,5 +50,22 @@ class TestSet(unittest.TestCase):
         m.eyes_off()
         self.assertEqual(message, mock_ws.message)
 
+    @patch('mycroftapi.create_connection')
+    def test_eyes_on(self, mock_create_conn):
+        # Create simple replacement websocket object and return it
+        # when creating sockets
+        mock_ws = MockWS()
+        mock_create_conn.return_value = mock_ws
+        # Test that init calls create_connection with correct param
+        m = MycroftAPI('127.0.0.1')
+        mock_create_conn.assert_called_with(
+            "ws://" + '127.0.0.1' + ":8181/core")
+        # Check that message bus message looks like what we expect
+        # Expected data to websocket
+        mycroft_type = '"enclosure.eyes.on"'
+        message = '{"type": ' + mycroft_type + '}'
+        m.eyes_on()
+        self.assertEqual(message, mock_ws.message)
+
 if __name__ == '__main__':
     unittest.main()
